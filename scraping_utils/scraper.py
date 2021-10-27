c6661bacbf1adf83696a66e73a26ba9c7a903a7c
Shubham Shetty <shubhamshett@umass.edu>
2021-10-27 19:30:57 -0400

    Added get_tweets_by_user

--- scraping_utils/scraper.py
+++ scraping_utils/scraper.py
@@ -20,8 +20,36 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth,wait_on_rate_limit=True)
 
+# Function to scrape twitter based on user
+def get_tweets_by_user(username, count, exclude_replies=True, include_rts=False, filename=None):
+    try:      
+        # Creation of query method using parameters
+        tweets = tweepy.Cursor(api.user_timeline,user_id=username, exclude_replies=True, include_rts=False).items(count)
+
+        # Pulling information from tweets iterable object
+        tweets_list = [[tweet.created_at, tweet.id, tweet.text] for tweet in tweets]
+
+        # Creation of dataframe from tweets list
+        # Add or remove columns as you remove tweet information
+        tweets_df = pd.DataFrame(tweets_list,columns=['Datetime', 'Tweet Id', 'Text'])
+
+        # Converting dataframe to CSV 
+        tweets_df.to_csv('{}-tweets.csv'.format(username), sep=',', index = False)
+        
+        # Set default filename
+        if filename == None:
+            filename = text_query.replace(' ', '_')
+        
+        # Converting dataframe to CSV 
+        tweets_df.to_csv(f'{OUTPUT_DIR}/{filename}.txt', sep=',', index = False)
+
+    except BaseException as e:
+          print('failed on_status,',str(e))
+          time.sleep(3)
+
+
 # Function to scrape twitter based on query
-def get_tweets(text_query, count, lang='en', result_type='mixed', filename=None, filter_rt=True):
+def get_tweets_by_query(text_query, count, lang='en', result_type='mixed', filename=None, filter_rt=True):
     try:
         # Filter retweets
         if filter_rt:
@@ -47,6 +75,7 @@ def get_tweets(text_query, count, lang='en', result_type='mixed', filename=None,
     except BaseException as e:
         print('failed on_status,',str(e))
         time.sleep(3)
+
 
 #if __name__=='__main__':
 #    text_query = "Asian Hate"
