ed79a8b4e793af0aa11699e9a9c567af7c5dbb63
Shubham Shetty <shubhamshett@umass.edu>
2021-10-28 13:45:08 -0400

    Generated text

--- scraping_utils/scraper.py
+++ scraping_utils/scraper.py
@@ -21,27 +21,31 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth,wait_on_rate_limit=True)
 
 # Function to scrape twitter based on user
-def get_tweets_by_user(username, count, exclude_replies=True, include_rts=False, filename=None):
+def get_tweets_by_user(username, count, include_rts=False, write_to_file=False, filename=None):
+    print("Helo")
     try:      
         # Creation of query method using parameters
-        tweets = tweepy.Cursor(api.user_timeline,user_id=username, exclude_replies=True, include_rts=False).items(count)
-
+        tweets = tweepy.Cursor(api.user_timeline,screen_name=username, include_rts=False, tweet_mode = 'extended').items(count)
+        print(tweets)
         # Pulling information from tweets iterable object
-        tweets_list = [[tweet.created_at, tweet.id, tweet.text] for tweet in tweets]
+        tweets_list = [[tweet.created_at, tweet.id, tweet.full_text] for tweet in tweets]
+        print(tweets_list)
 
         # Creation of dataframe from tweets list
         # Add or remove columns as you remove tweet information
         tweets_df = pd.DataFrame(tweets_list,columns=['Datetime', 'Tweet Id', 'Text'])
-
-        # Converting dataframe to CSV 
-        tweets_df.to_csv('{}-tweets.csv'.format(username), sep=',', index = False)
+        print(tweets_df)
         
-        # Set default filename
-        if filename == None:
-            filename = text_query.replace(' ', '_')
+        if write_to_file == True:
+            # Set default filename
+            if filename == None:
+                filename = f"file_{username.replace(' ', '_')}"
+            
+            # Converting dataframe to CSV 
+            tweets_df.to_csv(f'{OUTPUT_DIR}/{filename}.txt', sep=',', index = False)
         
-        # Converting dataframe to CSV 
-        tweets_df.to_csv(f'{OUTPUT_DIR}/{filename}.txt', sep=',', index = False)
+        else:
+            return tweets_df
 
     except BaseException as e:
           print('failed on_status,',str(e))
@@ -56,10 +60,10 @@ def get_tweets_by_query(text_query, count, lang='en', result_type='mixed', filen
             text_query = text_query + " -filter:retweets"
         
         # Creation of query method using parameters
-        tweets = tweepy.Cursor(api.search_tweets,q=text_query, lang=lang, result_type=result_type).items(count)
+        tweets = tweepy.Cursor(api.search_tweets,q=text_query, lang=lang, result_type=result_type, tweet_mode = 'extended').items(count)
      
         # Pulling information from tweets iterable object
-        tweets_list = [[tweet.created_at, tweet.id, tweet.text] for tweet in tweets]
+        tweets_list = [[tweet.created_at, tweet.id, tweet.full_text] for tweet in tweets]
      
         # Creation of dataframe from tweets list
         # Add or remove columns as you remove tweet information
@@ -78,6 +82,6 @@ def get_tweets_by_query(text_query, count, lang='en', result_type='mixed', filen
 
 
 #if __name__=='__main__':
-#    text_query = "Asian Hate"
+#    text_query = ""
 #    count = 20
 #    get_tweets(text_query, count, filename = "test", result_type="mixed")
